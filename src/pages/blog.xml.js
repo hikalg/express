135cfbf0b818632c7f660ca816372bac135572f3
Everest Luong <101264127+hikalg@users.noreply.github.com>
2026-04-29 14:40:43 +1000

    add rss

---
+++ src/pages/blog.xml.js
@@ -0,0 +1,18 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const blog = await getCollection('blog');
+  return rss({
+    title: `Everest's Blog`,
+    description: `Just Everest Luong (and sometimes Ery) yapping`,
+    site: context.site,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      link: `/blog/${post.data.shortid}/`,
+      pubDate: post.data.date,
+    })),
+    author: `everest@hikamarine.au`,
+    customData: `<language>en-au</language>`,
+  });
+}
